7a889a4c80b95320450e7a90e2625fdb19db1818
Rajesh <er.rajesh22@gmail.com>
2025-08-24 18:57:01 +0530

    add commands service

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+	[Route("api/c/[controller]")]
+	[ApiController]
+	public class PlatformsController : ControllerBase
+	{
+		public PlatformsController()
+		{
+			
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetPlatforms()
+		{
+			Console.WriteLine($"--> Incoming request to {nameof(CommandsService)}.{nameof(PlatformsController)}.{nameof(GetPlatforms)}.");
+			return Ok(new { Message = "This is the PlatformsController!" });
+		}
+	}
+}
